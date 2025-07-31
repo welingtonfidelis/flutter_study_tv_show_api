4d99f4601a564ad533421c56c0d78fe4e11129e4
Welington Fidelis <welingtonfidelis@gmail.com>
2025-07-31 10:11:43 -0300

    Include float action button to sort by name or rating;

--- lib/fav_tv_show_screen.dart
+++ lib/fav_tv_show_screen.dart
@@ -1,6 +1,7 @@
 import 'package:app3_series_api/tv_show_grid.dart';
 import 'package:app3_series_api/tv_show_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:provider/provider.dart';
 
 class FavTvShowScreen extends StatefulWidget {
@@ -11,9 +12,31 @@ class FavTvShowScreen extends StatefulWidget {
 }
 
 class _FavTvShowScreenState extends State<FavTvShowScreen> {
+  var sortByNameAsc = true;
+  var sortByRatingAsc = true;
+
+  void handleSort(SortType sortType) {
+    final tvShowModel = context.read<TvShowModel>();
+
+    if (sortType == SortType.nameAsc) {
+      tvShowModel.sortTvShows(sortType, sortByNameAsc);
+
+      setState(() {
+        sortByNameAsc = !sortByNameAsc;
+      });
+    } else if (sortType == SortType.ratingAsc) {
+      tvShowModel.sortTvShows(sortType, sortByRatingAsc);
+
+      setState(() {
+        sortByRatingAsc = !sortByRatingAsc;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    var tvShows = context.watch<TvShowModel>().tvShows;
+    final tvShowModel = context.watch<TvShowModel>();
+    var tvShows = tvShowModel.tvShows;
 
     return Container(
       padding: EdgeInsets.all(16),
@@ -25,6 +48,56 @@ class _FavTvShowScreenState extends State<FavTvShowScreen> {
           ),
           SizedBox(height: 16),
           Expanded(child: TvShowGrid(tvShows: tvShows)),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: SpeedDial(
+              icon: Icons.add,
+              activeIcon: Icons.close,
+              backgroundColor: Colors.blue,
+              children: [
+                SpeedDialChild(
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Icon(Icons.abc, size: 32), // ícone base
+                      Positioned(
+                        top: -2,
+                        right: -2,
+                        child: Icon(
+                          sortByNameAsc
+                              ? Icons.arrow_upward
+                              : Icons.arrow_downward,
+                          size: 12,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ],
+                  ),
+                  onTap: () => handleSort(SortType.nameAsc),
+                ),
+                SpeedDialChild(
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Icon(Icons.star, size: 32), // ícone base
+                      Positioned(
+                        top: -2,
+                        right: -2,
+                        child: Icon(
+                          sortByRatingAsc
+                              ? Icons.arrow_upward
+                              : Icons.arrow_downward,
+                          size: 12,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ],
+                  ),
+                  onTap: () => handleSort(SortType.ratingAsc),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
