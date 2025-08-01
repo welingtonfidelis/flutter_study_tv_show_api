55896ef270118b8614a25c35dbec734ec15f842e
Welington Fidelis <welingtonfidelis@gmail.com>
2025-08-01 09:46:58 -0300

    Create new functions to use database data;

--- lib/tv_show_service.dart
+++ lib/tv_show_service.dart
@@ -1,9 +1,80 @@
 import 'dart:convert';
 
+import 'package:app3_series_api/database_service.dart';
 import 'package:app3_series_api/tv_show_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:sqflite/sql.dart';
 
 class TvShowService {
+  late final DatabaseService _databaseService;
+  static final tableName = 'tv_shows';
+
+  TvShowService() {
+    _databaseService = DatabaseService();
+  }
+
+  Future<List<TvShow>> getAll() async {
+    final db = await _databaseService.database;
+
+    final List<Map<String, dynamic>> maps = await db.query(tableName);
+
+    return _convertToList(maps);
+  }
+
+  Future<TvShow?> getById(int id) async {
+    final db = await _databaseService.database;
+
+    final List<Map<String, dynamic>> maps = await db.query(
+      tableName,
+      where: 'id == ?',
+      whereArgs: [id],
+    );
+
+    if (maps.isNotEmpty) {
+      return _convertToList(maps)[0];
+    } else {
+      return null;
+    }
+  }
+
+  List<TvShow> _convertToList(List<Map<String, dynamic>> maps) {
+    return maps
+        .map(
+          (item) => TvShow(
+            id: item['id'] as int,
+            name: item['name'] as String? ?? 'N/A',
+            webChannel: item['webChannel'] as String? ?? 'N/A',
+            rating: (item['rating'] as num?)?.toDouble() ?? 0.0,
+            summary: item['summary'] as String? ?? 'N/A',
+            imageUrl: item['imageUrl'] as String? ?? '',
+          ),
+        )
+        .toList();
+  }
+
+  Future<void> insert(TvShow tvShow) async {
+    final db = await _databaseService.database;
+
+    await db.insert(
+      tableName,
+      tvShow.toJson(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<void> delete(int id) async {
+    final db = await _databaseService.database;
+
+    await db.delete(tableName, where: 'id == ?', whereArgs: [id]);
+  }
+
+  Future<bool> isFavorite(TvShow tvShow) async {
+    final selectedTvShow = await getById(tvShow.id);
+
+    return selectedTvShow != null;
+  }
+
+  // API
   Future<List<TvShow>> fetchTvShows(String query) async {
     final response = await http.get(
       Uri.parse('https://api.tvmaze.com/search/shows?q=$query'),
